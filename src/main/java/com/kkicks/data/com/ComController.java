2c435bb533cdce59b4e9d2dc0f0557e386133ae3
isaacshin92 <isaacshin92@gmail.com>
2022-02-21 23:29:08 +0900

    :refactoring, main index mapping, spring seurity

--- src/main/java/com/kkicks/data/com/ComController.java
+++ src/main/java/com/kkicks/data/com/ComController.java
@@ -1,10 +1,17 @@
 package com.kkicks.data.com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 // controller which handles basic page calls from the web front
 @Controller
 public class ComController {
+
+    @GetMapping(value="/")
+    public String index() {
+
+        return "index";
+    }
 
 }
