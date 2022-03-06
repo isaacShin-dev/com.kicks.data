cce121c288bdf48641097ebcb907a331ac4f1604
isaacshin92 <isaacshin92@gamil.com>
2022-03-06 16:58:49 +0900

    :include page :login, join page mapping :security

--- src/main/java/com/kkicks/data/userAccount/UserAccountController.java
+++ src/main/java/com/kkicks/data/userAccount/UserAccountController.java
@@ -6,12 +6,13 @@ import com.kkicks.data.core.domain.UserAccount;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // handles requests regarding userAccount /infos
 @Controller
-@RequestMapping("/user")
+
 public class UserAccountController {
     
     private UserAccountService userAccountService;
@@ -19,19 +20,30 @@ public class UserAccountController {
     ///////////////////////////
     //Member Join 
     ///////////////////////////
-    @RequestMapping(value = "/join")
+    @RequestMapping(value = "user/join")
     public ResponseEntity<?> memberJoin(UserAccount reqAccount){
- 
-
+        
+        
         UserAccount newUser = userAccountService.joinMember(reqAccount);
-
+        
         return ResponseEntity.ok().body(newUser);
     }
-    
+    @GetMapping("/join")
+    public String joinPage(){
+        return "user/join";
+    }
+
+    ///////////////////////////
+    //Member Join 
+    ///////////////////////////
+    @GetMapping("/login")
+    public String userLogin(){
+        return "user/login";
+    }
     ///////////////////////////
     //User Info  
     ///////////////////////////
-    @RequestMapping("/{userId}")
+    @RequestMapping("user/{userId}")
     public ResponseEntity<?> userInfo(@PathVariable("userId") UUID userId){
 
         return null; 
